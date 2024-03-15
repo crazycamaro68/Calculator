06638cbc9229163d962d139a0bca34574dcf289a
crazycamaro68 <153693685+crazycamaro68@users.noreply.github.com>
2024-03-14 20:47:48 -0400

    Created calculator funcunaltiy
    Made Add, Subtract, Multiply, and Divide functions.
    Made a function called operate that will select
    the proper operation and pass two values to it to
    be processed.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,35 @@
-alert("test");
+const add = (value1, value2) => {
+    let total = value1 + value2;
+    return total;
+}
+const sub = (value1, value2) =>{
+    let total = value1 - value2;
+    return total;
+}
+const mulitply = (value1, value2) =>{
+    let total = value1 * value2;
+    return total;
+}
+const divide = (value1, value2) =>{
+    let total = value1 / value2;
+    return total;
+}
+
+const operate = (opt, value1, value2) =>{
+    
+    if(opt == "add"){
+        let total = add(value1, value2);
+        return total;
+    }else if(opt == "sub"){
+        let total = sub(value1, value2);
+        return total;
+    }else if(opt == "mulitply"){
+        let total = mulitply(value1, value2);
+        return total;
+    }else if(opt =="divide"){
+        let total = divide(value1, value2);
+        return total;
+    }else{
+        alert("invalid");
+    }
+}
