ead4fcca8362c2e1941bb68c2942dcc0c625035b
crazycamaro68 <153693685+crazycamaro68@users.noreply.github.com>
2024-03-26 19:52:19 -0400

    Added functions for buttons
    Each number button adds the number pressed together and stores it, then
    when a operator button is pressed it then creates a secound value
    and runs the operate function with the stored values.
    
    Also made a clear button that resets all the values to default.

--- javascript.js
+++ javascript.js
@@ -1,35 +1,91 @@
-const add = (value1, value2) => {
+let value1;
+let value2;
+let opt;
+let joinedNumber;
+let final;
+
+
+function add(value1, value2){
     let total = value1 + value2;
     return total;
 }
-const sub = (value1, value2) =>{
+function sub(value1, value2){
     let total = value1 - value2;
     return total;
 }
-const mulitply = (value1, value2) =>{
+function mulitply(value1, value2){
     let total = value1 * value2;
     return total;
 }
-const divide = (value1, value2) =>{
+function divide(value1, value2){
     let total = value1 / value2;
     return total;
 }
 
-const operate = (opt, value1, value2) =>{
+function operate(opt, value1, value2){
     
-    if(opt == "add"){
+    if(opt == "+"){
         let total = add(value1, value2);
         return total;
-    }else if(opt == "sub"){
+    }else if(opt == "-"){
         let total = sub(value1, value2);
         return total;
-    }else if(opt == "mulitply"){
+    }else if(opt == "*"){
         let total = mulitply(value1, value2);
         return total;
-    }else if(opt =="divide"){
+    }else if(opt =="/"){
         let total = divide(value1, value2);
         return total;
     }else{
         alert("invalid");
     }
 }
+let newNumber = []
+let numObjects = document.querySelectorAll(".num");
+let operateObjects = document.querySelectorAll(".operate");
+let screen = document.querySelector("#output");
+let equalsButton = document.querySelector('#equals');
+let clearButton = document.querySelector('#clear');
+
+numObjects.forEach(elm => {elm.addEventListener("click", numberMaker)});
+operateObjects.forEach(element => {element.addEventListener("click", operationAssign)})
+equalsButton.addEventListener('click', numberMakerTwo);
+clearButton.addEventListener('click', clearScreen);
+
+function numberMaker(enteredNumber){
+    let pressedButton = enteredNumber.srcElement.textContent
+    newNumber.push(pressedButton);
+    if(newNumber.length > 8){
+        alert('Number is too long, please try again.')
+        newNumber = [];
+    }
+    joinedNumber = Number(newNumber.join(""));
+    screen.textContent = joinedNumber;
+    return joinedNumber;
+}
+function numberMakerTwo(){
+    value2 = joinedNumber;
+    final = operate(opt, value1, value2);
+    screen.textContent = final;
+    return final;
+}
+function operationAssign(enterOperator){
+    if(final == undefined){
+        value1 = joinedNumber;
+    }else{
+        value1 = final;
+    }
+    
+    joinedNumber = 0;
+    newNumber = [];
+    screen.textContent = joinedNumber;
+    opt = enterOperator.srcElement.textContent
+    return value1, opt;
+}
+function clearScreen(){
+    value1, value2, opt = 0;
+    final = undefined;
+    newNumber = [];
+    screen.textContent = 0;
+}
+
